976951b6ce70711fa811b9b81ff318f23b69da58
Bart Römgens <bart.romgens@gmail.com>
2017-06-23 15:07:43 +0200

    create initial paper.js example

---
+++ paperexample.js
@@ -0,0 +1,10 @@
+// Create a Paper.js Path to draw a line into it:
+var path = new Path();
+// Give the stroke a color
+path.strokeColor = 'black';
+var start = new Point(100, 100);
+// Move to start and draw a line from there
+path.moveTo(start);
+// Note the plus operator on Point objects.
+// PaperScript does that for us, and much more!
+path.lineTo(start + [ 100, -50 ]);
